e6336fc4b679955b974359498e737c9fac4eee77
iceny <iceny.cn@gmail.com>
2017-08-01 17:14:52 +0800

    update database class

--- server/db.php
+++ server/db.php
@@ -85,5 +85,124 @@ class DB{
         return mysqli_free_result($query); //释放结果集内存
     }
 
+    public function getRow($sql,$type='assoc'){
+        $query = $this->query($sql);
+        if(!in_array($type,array('assoc','array','row'))){
+            die('mysql_query error');
+        }
+        $funcname = "mysql_fetch_".$type;
+        return $funcname($query);
+    }
+
+    //获取多条数据，二维数组
+    public function getAll($table,$where,$fields='*',$order='',$skip=0,$limit=1000){
+        if(is_array($where)){
+            foreach($where as $key=>$val){
+                if(is_numeric($val)){
+                    $condition = $key.'='.$val;
+                }else{
+                    $condition = $key.'=\"'.$val.'\"';
+                }
+            }
+        }else{
+            $condition = $where;
+        }
+        if(!empty($order)){
+            $order = " order by ".$order;
+        }
+        $sql = "select $fields from $table where $condition $order limit $skip,$limit";
+        $query = $this->query($sql);
+        $list = array();
+        while($r=$this->getFormSource($query)){
+            $list[] = $r;
+        }
+        return $list;
+    }
+
+    //定义添加数据的方法
+    public function insert($table,$data){
+        //遍历数组得到每个字段和字段的值
+        $key_str='';
+        $v_str='';
+        foreach($data as $key=>$v){
+            //$key的值是每个字段s一个字段所对应的值
+            $key_str.=$key.',';
+            $v_str.="'$v',";
+        }
+        $key_str = trim($key_str,',');
+        $v_str = trim($v_str,',');
+        //判断数据是否为空
+        $sql = "insert into $table ($key_str) values ($v_str)";
+        $this->query($sql);
+        //返回上一次增加操作产生的ID值
+        return $this->getInsertid();
+    }
+
+    //删除一条数据
+    public function deleteOne($table,$where){
+        if(is_array($where)){
+            foreach($where as $key=>$val){
+                $condition = $key.'='.$val;
+            }
+        }else{
+            $condition = $where;
+        }
+        $sql = "delete from $table where $condition";
+        $this->query($sql);
+        //返回受影响的行数
+        return mysqli_affected_rows($this->link);
+    }
+
+    //删除多条数据
+    public function deleteAll($table,$where){
+        if(is_array($where)){
+            foreach($where as $key=>$val){
+                if(is_array($val)){
+                    $condition = $key.' in ('.implode(',',$val).')';
+                }else{
+                    $condition = $key. '=' .$val;
+                }
+            }
+        }else{
+            $condition = $where;
+        }
+        $sql = "delete from $table where $condition";
+        $this->query($sql);
+        //返回受影响的行数
+        return mysqli_affected_rows($this->link);        
+    }
+
+    //修改操作description
+    public function update($table,$data,$where,$limit=0){
+        //遍历数组，得到每一个字段和字段的值
+        $str = '';
+        foreach($data as $key=>$v){
+            $str.="$key='$v',";
+        }
+        $str = rtrim($str,',');
+        if(is_array($where)){
+            foreach($where as $key=>$val){
+                if(is_array($val)){
+                    $condition = $key.' in ('.implode(',',$val) .')';
+                }else{
+                    $condition = $key. '=' .$val;
+                }
+            }
+        }else{
+            $condition = $where;
+        }
+        if(!empty($limit)){
+            $limit = " limit ".$limit;
+        }else{
+            $limit='';
+        }
+        //修改sql语句
+        $sql = "update $table set $str where $condition $limit";
+        $this->query($sql);
+        //返回受影响的行数
+        return mysqli_affected_rows($this->link);
+    }
+
+
 
 }
